de8ce131e1b8f1cf6c50e89949015f6882ab5e3c
dvdrtrgn <dvdrtrgn@gmail.com>
2014-02-03 17:04:51 -0600

    refactor main to handle binding scroll and filler

--- scripts/main.js
+++ scripts/main.js
@@ -4,11 +4,12 @@
 var $W, $$;
 
 var Main = (function (W, $) {
-    var C, self, name, desktop;
+    var C, self, name, html, wrap;
 
     C = W.console;
     name = 'Main';
-    desktop = false;
+    html = $('html');
+    wrap = $('#Wrap');
 
     // Cache the Window object
     $W = $(W);
@@ -21,59 +22,107 @@ var Main = (function (W, $) {
         return W.debug >= (n || 0);
     }
 
-    function _updatePlatform() {
-        if (desktop) {
-            $('html').addClass('desktop');
-            $('html').removeClass('mobile');
-        } else {
-            $('html').removeClass('desktop');
-            $('html').addClass('mobile');
+    if ('STICKY') {
+        var currentSection = 'Wrap';
+
+        function _stickTo() {
+            Util.scroll(currentSection, $$.OFF);
+            //
+            _debug(2) && C.error('_stickTo', currentSection[0].id);
+        }
+
+        $(W).scroll(_.debounce(_stickTo, 1111));
+
+        function _sectionStick(e, showing, h, vsides) {
+            var my = $(this);
+            //
+            if (showing) {
+                _debug() && C.debug('_sectionStick', my.parent().parent()[0].id, vsides);
+                //
+                if (vsides === 'both' || (vsides === 'top' && my.is('.sticky'))) {
+                    currentSection = my.closest('section');
+                }
+            }
         }
     }
-    function _isMobile() {
-        desktop = ($$.port.all.wide > 960);
-        return !desktop;
-    }
-    function _setPlatform() {
-        // desktop changed by _isMobile (=== order is important)
-        if (desktop === _isMobile()) {
-            _updatePlatform();
-            C.warn('_isMobile change');
+    /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+    if ('PLATFORM') {
+        var desktop = false;
+
+        function _updatePlatform() {
+            if (desktop) {
+                $('html').addClass('desktop');
+                $('html').removeClass('mobile');
+            } else {
+                $('html').removeClass('desktop');
+                $('html').addClass('mobile');
+            }
         }
-        return desktop ? 'desktop' : 'mobile';
+
+        function _isMobile() {
+            desktop = ($$.port.all.wide > 960);
+            return !desktop;
+        }
+
+        function _setPlatform() {
+            // desktop changed by _isMobile (=== order is important)
+            if (desktop === _isMobile()) {
+                _updatePlatform();
+                C.warn('_isMobile change');
+            }
+            return desktop ? 'desktop' : 'mobile';
+        }
     }
+    /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+    function _bubbleWrap() {
+        $(this).children().not('.ribbon').wrapAll('<div class="filler">');
+    }
+
+    function _activeSection(i) {
+        var bg = new Bg(this, $$.port);
+        //
+        $W.on('scroll resize', function () {
+            bg.redraw();
+
+            if (!bg.isShowing()) {
+                _debug(2) && C.log('offscreen', i, bg._css.backgroundPosition);
+            } else {
+                _debug(2) && C.log('offscreen', i, bg._css.backgroundPosition);
+            }
+        });
+    }
+
+    function _bindAll() {
+        html.on('click', '.touch', function () {
+            $(this).toggleClass('hover');
+        });
+
+        wrap.find('section') //
+        .each(_activeSection) //
+        .each(_bubbleWrap) //
+        .find('.filler > *').on('inview', _sectionStick);
+
+        $W.on('resize', _setPlatform).scroll();
+    }
+    /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
     function _init() {
         _debug() && C.log([name]);
 
         $$.port = new Port($W);
-
-        $('#Wrap section').each(function (i) {
-            var el, bg;
-            //
-            el = this;
-            bg = new Bg(el, $$.port);
-            // cache em
-            $W.on('scroll resize', function () {
-                bg.redraw();
-
-                _setPlatform();
-
-                if (!bg.isShowing()) {
-                    _debug(2) && C.log('offscreen', i, bg._css.backgroundPosition);
-                } else {
-                    _debug(2) && C.log('offscreen', i, bg._css.backgroundPosition);
-                }
-            });
-        });
-
-        // touch
-        $W.scroll();
-
         $$.gallery = W.Gallery && Gallery.init();
-        $$.url     = W.Url     && Url.init();
-        $$.debug   = W.Debug   && Debug.init();
+        $$.url = W.Url && Url.init();
+        $$.debug = W.Debug && Debug.init();
 
+        _bindAll();
+        Util.scroll('#Wrap');
+        W.setTimeout(Url.clear, 999);
+
+        wrap.fitText(10, {
+            'minFontSize': 7
+        });
         return self;
     }
 
--- scripts/tmp.js
+++ scripts/tmp.js
@@ -1,64 +1,21 @@
 /*jslint es5:true, white:false */
 /*globals $$, $W, Util, jQuery, window, _ */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-// TODO: abort parallax at 800px wide
-// TODO: smooth scroll
-
 var TMP = (function (W, $) {
     var C, self, name;
 
     name = 'TMP';
     C = W.console;
-    currentSection = 'Wrap';
 
     /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
     function _debug(n) {
         return W.debug >= (n || 0);
-    }
-
-    function _stickTo() {
-        Util.scroll(currentSection, $$.OFF);
-        //
-        _debug(2) && C.error('_stickTo', currentSection[0].id);
-    }
-
-    $(window).scroll(_.debounce(_stickTo, 1111));
-
-    function _sectionStick(e, showing, h, vsides) {
-        var my = $(this);
-        //
-        if (showing){
-            _debug() && C.debug('_sectionStick', my.parent().parent()[0].id, vsides);
-            //
-            if (vsides === 'both' || (vsides === 'top' && my.is('.sticky'))) {
-                currentSection = my.closest('section');
-            }
-        }
     }
 
     function _init() {
         _debug() && C.log([name]);
 
-        var html, wrap;
-        //
-        html = $('html');
-        wrap = $('#Wrap');
-
-        html.on('click', '.touch', function () {
-            $(this).toggleClass('hover');
-        });
-        wrap.fitText(10, {
-            'minFontSize': 7
-        });
-        //
-        C.debug(wrap.find('section').each(function () {
-            $(this).children().not('.ribbon').wrapAll('<div class="filler">');
-        }));
-        //
-        $('.filler > *').on('inview', _sectionStick);
-        Util.scroll('#Wrap');
-
-        W.setTimeout(Url.clear, 999);
 
         return self;
     }
