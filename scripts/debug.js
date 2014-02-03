32b74484ab68608ef2fa4996d59e2847f7b727db
dvdrtrgn <dvdrtrgn@gmail.com>
2014-02-03 16:21:32 -0600

    filter out unused gold

--- scripts/debug.js
+++ scripts/debug.js
@@ -1,5 +1,5 @@
 /*jslint es5:true, white:false */
-/*globals $$, Marks, Menu, Pager, jQuery, window */
+/*globals $$, Marks, jQuery, window */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 var Debug = (function (W, $) {
     var C, self, name, html, wrap;
@@ -33,8 +33,6 @@ var Debug = (function (W, $) {
 
         _bind();
         $$.marks   = W.Marks   && Marks.init();
-        $$.menu    = W.Menu    && Menu.init();
-        // $$.pager   = W.Pager   && Pager.init();
 
         return self;
     }
